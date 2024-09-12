11157271f5040f220e60035d4c665170bfb014b5
Bill Mills <bill.mills@linaro.org>
2024-09-11 20:50:01 -0400

    demo2a: scripts to start the two qemus
    demo2a is "Running Virtio-MSG with the Linux-user msg-bus using two
    QEMU instances" [1]
    
    These scripts are as is from the writeup except:
    * run-B.sh: modified kernel and initrd paths and names
    * run-B.sh: reordered a bit to put all virtio-msg stuff last
    
    [1] https://github.com/edgarigl/qemu/blob/edgar/virtio-msg/docs/devel/virtio-msg/virtio-msg.rst#running-virtio-msg-with-the-linux-user-msg-bus-using-two-qemu-instances
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

---
+++ demo2a.d/scripts/run-B.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+./build/qemu-msg-install/bin/qemu-system-aarch64 \
+     -M virt -m 2G -cpu cortex-a72 \
+     -object memory-backend-file,id=mem,size=2G,mem-path=/dev/shm/qemu-ram,share=on \
+     -machine memory-backend=mem \
+     -serial mon:stdio -display none \
+     -kernel ./build/Image \
+     -initrd ./build/disk/virtio_msg_rootfs.cpio.gz \
+     -append "console=ttyAMA0" \
+     -chardev socket,id=chr0,path=linux-user.socket \
+     -device virtio-msg-proxy-driver-pci,virtio-id=0x1 \
+     -device virtio-msg-bus-linux-user,name=linux-user,chardev=chr0
