577cb4d5903b05944cf3aa340575b4c1c08c83e3
Bill Mills <bill.mills@linaro.org>
2024-09-13 17:56:28 -0400

    demo1: allow the demo1.sh script on dom0 to run multiple times
    * Do the per-boot setup only once per boot.
    * Kill the rust jobs and restart then next time
      After domu has run and shutdown, the rust jobs are still running but
      trying to run the domu again crashes.  If we kill the jobs and restart
      them, this does not happen.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- mixins/dom0/root/demo1.sh
+++ mixins/dom0/root/demo1.sh
@@ -1,16 +1,22 @@
 #/bin/sh
 
-set -e 
+set -e
 
-# start the xen daemons
-/etc/init.d/xencommons start
+# once per dom0 boot
+# /run is volatile and does not persist from boot to boot
+if [ ! -e /run/demo1-setup.done ]; then
+	# start the xen daemons
+	/etc/init.d/xencommons start
 
-# should only show Domain-0
-xl list
+	# should only show Domain-0
+	xl list
 
-# inform i2c controller of device but don't bind a driver
-echo ds1338 0x20 > /sys/bus/i2c/devices/i2c-0/new_device
-echo 0-0020 > /sys/bus/i2c/devices/0-0020/driver/unbind
+	# inform i2c controller of device but don't bind a driver
+	echo ds1338 0x20 > /sys/bus/i2c/devices/i2c-0/new_device
+	echo 0-0020 > /sys/bus/i2c/devices/0-0020/driver/unbind
+
+	touch /run/demo1-setup.done
+fi
 
 # start the i2c backend
 RUST_BACKTRACE=full ./vhost-device-i2c -s /root/i2c.sock -c 1 -l "90c0000.i2c:32" &
@@ -18,3 +24,6 @@ RUST_BACKTRACE=full ./xen-vhost-frontend --socket-path /root/ &
 
 # now start the quest
 xl create -c domu.conf
+
+# kill all jobs of this shell (hopefully just the two backend processes)
+kill $(jobs -p)
