6214534a5e9113b5ec5c6c394de63f51295ee4a7
Bill Mills <bill.mills@linaro.org>
2024-09-13 09:17:46 -0400

    demo1: produce full stack trace if rust apps crash
    Enable a full stack dump if the apps crash.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- mixins/dom0/root/demo1.sh
+++ mixins/dom0/root/demo1.sh
@@ -13,8 +13,8 @@ echo ds1338 0x20 > /sys/bus/i2c/devices/i2c-0/new_device
 echo 0-0020 > /sys/bus/i2c/devices/0-0020/driver/unbind
 
 # start the i2c backend
-./vhost-device-i2c -s /root/i2c.sock -c 1 -l "90c0000.i2c:32" &
-./xen-vhost-frontend --socket-path /root/ &
+RUST_BACKTRACE=full ./vhost-device-i2c -s /root/i2c.sock -c 1 -l "90c0000.i2c:32" &
+RUST_BACKTRACE=full ./xen-vhost-frontend --socket-path /root/ &
 
 # now start the quest
 xl create -c domu.conf
