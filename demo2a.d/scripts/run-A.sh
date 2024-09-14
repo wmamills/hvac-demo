97ca594e131dbb837322820fa9bdb2dbf5250cc2
Bill Mills <bill.mills@linaro.org>
2024-09-14 08:58:22 -0400

    demo2a: use local file-system for qemu-ram memory file
    When we were using /dev/shm the real qemu (run-B.sh) was crashing.
    The file was the right size and owned by me.
    Perhaps something strange about /dev/shm fs in a container.
    
    Moving to use the local disk for the file worked fine.
    
    Also delete the memory files before we start and after we finish.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo2a.d/scripts/run-A.sh
+++ demo2a.d/scripts/run-A.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-rm -fr queue-linux-user-d*
+rm -f ./queue-linux-user-d*
+rm -f ./qemu-ram
 
 ./build/qemu-msg-install/bin/qemu-system-aarch64 \
      -M x-virtio-msg -m 2G -cpu cortex-a72 \
-     -object memory-backend-file,id=mem,size=2G,mem-path=/dev/shm/qemu-ram,share=on \
+     -object memory-backend-file,id=mem,size=2G,mem-path=./qemu-ram,share=on \
      -machine memory-backend=mem \
      -chardev socket,id=chr0,path=linux-user.socket,server=on,wait=false \
      -serial mon:stdio -display none \
@@ -12,3 +13,6 @@ rm -fr queue-linux-user-d*
      -device virtio-net-device,netdev=net0,iommu_platform=on \
      -netdev user,id=net0 \
      -object filter-dump,id=f0,netdev=net0,file=net.pcap
+
+rm -f ./queue-linux-user-d*
+rm -f ./qemu-ram
