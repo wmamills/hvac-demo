e0b88ef029830a4eb1f54c6aec31bf5a332b77fa
Bill Mills <bill.mills@linaro.org>
2024-09-14 12:50:23 -0400

    demo2a: add top level demo script and adjust for multi-qemu
    * Add the top level demo script to call multi-qemu
    * adjust the qemu run scripts to use BASE_DIR
    * put the net.pcap in the LOGS dir
    * add a cleanup script to remove the memory files
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo2a.d/scripts/run-B.sh
+++ demo2a.d/scripts/run-B.sh
@@ -1,12 +1,20 @@
 #/bin/bash
 
-./build/qemu-msg-install/bin/qemu-system-aarch64 \
+# assume we are being run from the base directory if not defined
+: ${BASE_DIR:=.}
+: ${LOGS:=.}
+: ${TMPDIR:=.}
+
+echo "wait for device model to start"
+sleep 2
+
+$BASE_DIR/build/qemu-msg-install/bin/qemu-system-aarch64 \
      -M virt -m 2G -cpu cortex-a72 \
      -object memory-backend-file,id=mem,size=2G,mem-path=./qemu-ram,share=on \
      -machine memory-backend=mem \
      -serial mon:stdio -display none \
-     -kernel ./build/Image \
-     -initrd ./build/disk/virtio_msg_rootfs.cpio.gz \
+     -kernel $BASE_DIR/build/Image \
+     -initrd $BASE_DIR/build/disk/virtio_msg_rootfs.cpio.gz \
      -append "console=ttyAMA0" \
      -chardev socket,id=chr0,path=linux-user.socket \
      -device virtio-msg-proxy-driver-pci,virtio-id=0x1 \
