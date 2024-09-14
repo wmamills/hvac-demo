60fa4c86faf0426c4c785cc582ce88e59d494ba9
Bill Mills <bill.mills@linaro.org>
2024-09-14 12:50:23 -0400

    demo2a: add top level demo script and adjust for multi-qemu
    * Add the top level demo script to call multi-qemu
    * adjust the qemu run scripts to use BASE_DIR
    * put the net.pcap in the LOGS dir
    * add a cleanup script to remove the memory files
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

---
+++ demo2a.d/scripts/cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# normally the demo exits via qemu2 exiting via powerdown (or host shell exit)
+# when either of those happen, the qemu1 pane is killed
+# this does not give run-A.sh to cleanup the memory files so we do it here
+rm -f ./queue-linux-user-d*
+rm -f ./qemu-ram
