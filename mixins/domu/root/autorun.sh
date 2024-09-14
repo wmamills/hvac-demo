2a19694467eb98a3264264288829ea3154879538
Bill Mills <bill.mills@linaro.org>
2024-09-14 15:13:37 -0400

    demo1: trigger autorun from kernel command line
    * If autorun for busybox/domu look for autorun options on the
      kernel command line.
    * Use the right parameter for domu.conf kernel command line
    * enable autorun of ./demo1.sh and set a non-default time to test
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- mixins/domu/root/autorun.sh
+++ mixins/domu/root/autorun.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
-START_TIME=3
-STOP_TIME=5
+get_cmdline_arg() {
+	PARAM=$1
+	DEF_VALUE=$2
+
+	# sed, like perl, is a write only language
+	# first we grep for the param= to make sure it is there as the sed line
+	# won't work if it is not
+	# then the sed line uses # as the s command delimiter
+	# ^.* matches from the start of line until the PARAM= part
+	# $PARM= matches the parameter name and the equal sign
+	# \\ is needed for each \ as we have used double quotes to get $PARAM
+	# \( and later \) make a group of the value part
+	# [^[:space:]])* is the value match part that is group 1
+	#    it matches any number of characters up to the next whitespace
+	# .*$ matches anything till the end of string
+	# \$ is needed because of the double quotes
+	# The replacement says
+	#    replace everything we matched (the whole line)
+	#    with group 1 (the VALUE)
+	if grep -q "$PARAM=" /proc/cmdline; then
+		VALUE=$(sed -e "s#^.*$PARAM=\\([^[:space:]]*\\).*\$#\\1#" /proc/cmdline)
+		if [ -n $VALUE ]; then
+			echo $VALUE
+			return
+		fi
+	fi
+	echo $DEF_VALUE
+}
 
 delay() {
 	DELAY=$1
@@ -11,15 +37,23 @@ delay() {
 	done
 }
 
-echo "To stop autorun, hit ctrl-c in the next $START_TIME seconds"
-delay $START_TIME
+AUTORUN=$(get_cmdline_arg autorun "")
+AUTORUN_DELAY=$(get_cmdline_arg autorun_delay 5)
 
-./demo1.sh
+echo "/proc/cmdline: $(cat /proc/cmdline)"
+echo "AUTORUN=$AUTORUN AUTORUN_DELAY=$AUTORUN_DELAY"
 
-echo "To stop poweroff, hit ctrl-c in the next $STOP_TIME seconds"
-delay $STOP_TIME
+if [ -z "$AUTORUN" ]; then
+	exit
+fi
 
-echo "DomU guest poweroff"
+echo "To stop autorun, hit ctrl-c in the next $AUTORUN_DELAY seconds"
+delay $AUTORUN_DELAY
+
+"$AUTORUN"
+
+echo "To stop poweroff, hit ctrl-c in the next $AUTORUN_DELAY seconds"
+delay $AUTORUN_DELAY
+
+echo "busybox poweroff"
 poweroff
-
-
