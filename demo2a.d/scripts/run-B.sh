c08ea41ef95c25a7c30d83bcbfaa028877ccd8c6
Bill Mills <bill.mills@linaro.org>
2024-09-14 08:58:22 -0400

    demo2a: use local file-system for qemu-ram memory file
    When we were using /dev/shm the real qemu (run-B.sh) was crashing.
    The file was the right size and owned by me.
    Perhaps something strange about /dev/shm fs in a container.
    
    Moving to use the local disk for the file worked fine.
    
    Also delete the memory files before we start and after we finish.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo2a.d/scripts/run-B.sh
+++ demo2a.d/scripts/run-B.sh
@@ -2,7 +2,7 @@
 
 ./build/qemu-msg-install/bin/qemu-system-aarch64 \
      -M virt -m 2G -cpu cortex-a72 \
-     -object memory-backend-file,id=mem,size=2G,mem-path=/dev/shm/qemu-ram,share=on \
+     -object memory-backend-file,id=mem,size=2G,mem-path=./qemu-ram,share=on \
      -machine memory-backend=mem \
      -serial mon:stdio -display none \
      -kernel ./build/Image \
