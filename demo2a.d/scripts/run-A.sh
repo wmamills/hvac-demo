45ebc22db798b14f6a765100bac53cb63c166a4c
Bill Mills <bill.mills@linaro.org>
2024-09-14 12:50:23 -0400

    demo2a: add top level demo script and adjust for multi-qemu
    * Add the top level demo script to call multi-qemu
    * adjust the qemu run scripts to use BASE_DIR
    * put the net.pcap in the LOGS dir
    * add a cleanup script to remove the memory files
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo2a.d/scripts/run-A.sh
+++ demo2a.d/scripts/run-A.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# if we are not run under multi-qemu, just fake this stuff out
+: ${BASE_DIR:=.}
+: ${LOGS:=.}
+: ${TMPDIR:=.}
+
 rm -f ./queue-linux-user-d*
 rm -f ./qemu-ram
 
-./build/qemu-msg-install/bin/qemu-system-aarch64 \
+$BASE_DIR/build/qemu-msg-install/bin/qemu-system-aarch64 \
      -M x-virtio-msg -m 2G -cpu cortex-a72 \
      -object memory-backend-file,id=mem,size=2G,mem-path=./qemu-ram,share=on \
      -machine memory-backend=mem \
@@ -12,7 +17,7 @@ rm -f ./qemu-ram
      -device virtio-msg-bus-linux-user,name=linux-user,chardev=chr0,memdev=mem,mem-offset=0x40000000 \
      -device virtio-net-device,netdev=net0,iommu_platform=on \
      -netdev user,id=net0 \
-     -object filter-dump,id=f0,netdev=net0,file=net.pcap
+     -object filter-dump,id=f0,netdev=net0,file=$LOGS/net.pcap
 
 rm -f ./queue-linux-user-d*
 rm -f ./qemu-ram
