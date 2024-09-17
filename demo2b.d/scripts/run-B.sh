af01d0b2cbb9379e45073de2576a8fdda3f51fb7
Bill Mills <bill.mills@linaro.org>
2024-09-17 09:18:32 -0400

    demo2b: add a copy of the debian disk to each base level qemu
    Attach to the machines but not used yet.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo2b.d/scripts/run-B.sh
+++ demo2b.d/scripts/run-B.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+NAME=demo2b-B
 
 # include the common variable settings
 ME_ABS=$(readlink -f $0)
 MY_DIR=$(dirname $ME_ABS)
 . $MY_DIR/common-vars.sh
+
+if [ ! -e $IMAGES/${NAME}-disk.qcow2 ]; then
+	echo "make a copy of the debian disk image for $NAME"
+	cp $IMAGES/disk.qcow2 $IMAGES/${NAME}-disk.qcow2
+fi
 
 echo booti 0x42000000 - 0x44000000
 echo booti 0x47000000 - 0x44000000
