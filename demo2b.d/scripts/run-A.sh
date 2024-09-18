75c804dc5432b01b9aa22e246aeac5d1969ddc5b
Bill Mills <bill.mills@linaro.org>
2024-09-18 14:51:42 -0400

    demo2b: move disk copy to common setup and delay qemu2
    To ensure that qemu1 always gets VMID 0, move the disk copy to the shared
    setup state.
    
    Then in qemu2 delay creating the qemu instance for a few seconds.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo2b.d/scripts/run-A.sh
+++ demo2b.d/scripts/run-A.sh
@@ -11,11 +11,6 @@ ME_ABS=$(readlink -f $0)
 MY_DIR=$(dirname $ME_ABS)
 . $MY_DIR/common-vars.sh
 
-if [ ! -e $IMAGES/${NAME}-disk.qcow2 ]; then
-	echo "make a copy of the debian disk image for $NAME"
-	cp $IMAGES/disk.qcow2 $IMAGES/${NAME}-disk.qcow2
-fi
-
 if [ "$MODE" == "U_BOOT" ]; then
     echo booti 0x42000000 - 0x44000000
     echo booti 0x47000000 - 0x44000000
--- demo2b.d/scripts/setup.sh
+++ demo2b.d/scripts/setup.sh
@@ -5,6 +5,15 @@ ME_ABS=$(readlink -f $0)
 MY_DIR=$(dirname $ME_ABS)
 . $MY_DIR/common-vars.sh
 
+#set -x
+
+for NAME in demo2b-A demo2b-B; do
+	if [ ! -e $IMAGES/${NAME}-disk.qcow2 ]; then
+		echo "make a copy of the debian disk image for $NAME"
+		cp $IMAGES/disk.qcow2 $IMAGES/${NAME}-disk.qcow2
+	fi
+done
+
 # make the dtb
 (cd $TEST_DIR/dts; make)
 
