9ad87ad43ebccd8345a7a548233c89ca47bc92b2
Bill Mills <bill.mills@linaro.org>
2024-09-19 17:26:30 -0400

    demo2b: move debian mixins back to demo2.d sub-tree
    The global debian mixins level was getting crowded so move these back to
    the demo2b.d tree.  They will be combined at build time.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo2b.d/mixins/debian/root/demo2b/demo2b-A-kvm.sh
+++ demo2b.d/mixins/debian/root/demo2b/demo2b-A-kvm.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#/bin/bash
 
 MY_DIR=$(dirname $0)
 
@@ -9,16 +9,10 @@ set -e
 mark_not_ready
 install_common
 install_qemu_deps
-install_xen xen-upstream qemu-xen
 vfio_setup
-xen_startup
-
-# should only show Domain-0
-xl list
 
 mark_ready
 $MY_DIR/guest-qemu-run-msg.sh 0x01
 
-# stop the poweroff until it is working
-bash
-
+# stop for debug
+#bash
--- demo2b.d/mixins/debian/root/demo2b/demo2b-A-xen.sh
+++ demo2b.d/mixins/debian/root/demo2b/demo2b-A-xen.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#/bin/bash
 
 MY_DIR=$(dirname $0)
 
@@ -9,10 +9,16 @@ set -e
 mark_not_ready
 install_common
 install_qemu_deps
+install_xen xen-upstream qemu-xen
 vfio_setup
+xen_startup
+
+# should only show Domain-0
+xl list
 
 mark_ready
 $MY_DIR/guest-qemu-run-msg.sh 0x01
 
 # stop for debug
 #bash
+
