0ca883d8b947fcd9537a04e19a0cff6e7e1b35dc
Bill Mills <bill.mills@linaro.org>
2024-09-18 20:22:02 -0400

    demo2b: adjust demo scripts to this environment
    * In our command line, the doorbell ivshmem is slot 3 and the whole memory
      share ivshmem is slot 4
    * Call VMID R_VMID (for remote) and use it in both scripts the same.
    * Decrease system memory size to 1G not 4G (less stress on host)
    * Use our location for qemu binary
    * kvm: use our rootfs name
    * kvm: kvm quest memory smaller to fit in qemu system memory
    * kvm: simplify command line for our rootfs
    * kvm: pass the autoboot script name
    * xen: decrease ram of quest
    * xen: adjust command line
    * xen: adjust file names
    
    NOTE: The xen configuration is untested at the moment
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- mixins/debian/root/demo2b/guest-qemu-run-virt.sh
+++ mixins/debian/root/demo2b/guest-qemu-run-virt.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 #MACHINE="-M virt,memory-backend=foo.ram"
-QEMU=qemu-system-aarch64
+QEMU=/opt/qemu-msg/bin/qemu-system-aarch64
 MACHINE="-M virt"
 KERNEL=${HOME}/Image
-ROOTFS=${HOME}/core-image-minimal-qemuarm64.rootfs.cpio.gz
+ROOTFS=${HOME}/demo2b-rootfs.cpio.gz
+R_VMID=$1
+shift
 
-DEV_DOORBELL=0000:00:02.0
+DEV_DOORBELL=0000:00:03.0
 
 set -x
-${QEMU} ${MACHINE} -m 2G -cpu host -accel kvm                   \
+${QEMU} ${MACHINE} -m 512M -cpu host -accel kvm                   \
         -serial mon:stdio -display none                             \
         -kernel ${KERNEL}                                       \
         -initrd ${ROOTFS}                                       \
-        -append "rdinit=/sbin/init console=ttyAMA0 lpj=100"     \
+        -append "console=ttyAMA0 autorun=./demo2b.sh"     \
         -device virtio-msg-proxy-driver-pci,virtio-id=0x1 \
-        -device virtio-msg-bus-ivshmem,dev=${DEV_DOORBELL},iommu=linux-proc-pagemap,remote-vmid=0x1C   \
+        -device virtio-msg-bus-ivshmem,dev=${DEV_DOORBELL},iommu=linux-proc-pagemap,remote-vmid=${R_VMID} \
         "$@"
 
 exit 0
