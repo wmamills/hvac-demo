dc94a47e110913d07d99fb207c097350a4cb0778
Bill Mills <bill.mills@linaro.org>
2024-10-09 00:11:22 -0400

    demo3: first draft of target test script
    Check for uio and then run the test program.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo3.d/mixins/debian/root/demo3/demo3.sh
+++ demo3.d/mixins/debian/root/demo3/demo3.sh
@@ -4,5 +4,17 @@ MY_DIR=$(dirname $0)
 
 set -e
 
-# stop for debug
-bash
+fail() {
+	echo "***** TEST FAILED, " "$@"
+}
+
+test -c /dev/uio0 || fail no uio0 device
+test -e /sys/class/uio/uio0/maps/map0/name || fail no sysfs for map0
+test -e /sys/class/uio/uio0/maps/map1/name || fail no sysfs for map1
+cat /sys/class/uio/uio0/maps/map*/name
+
+for i in $(seq 1 3); do
+	./zephyr_ivshmem_test /dev/uio0 0 || fail ivshmem_test failed
+done
+
+echo "***** TEST PASSED, uio ivshmem test passed"
