6365a48d967dc297568240e85ddce093f4e8bf52
Bill Mills <bill.mills@linaro.org>
2024-10-19 12:30:53 -0400

    demo2: fix comment in guest script
    This comment was wrong and misleading.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo2b.d/mixins/qemu2-guest/root/demo2b.sh
+++ demo2b.d/mixins/qemu2-guest/root/demo2b.sh
@@ -11,7 +11,7 @@ modprobe virtio-mmio
 
 ifconfig -a
 
-# In xen dom0, eth0 is the only nic and the one we want
+# In xen domU, eth0 is the only nic and the one we want
 ifconfig eth0 up
 
 # The virtio-msg device model running in qemu1 is using qemu user mode
