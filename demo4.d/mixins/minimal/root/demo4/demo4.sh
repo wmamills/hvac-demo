630347294ef199816103a929949c2171a6679d7b
Bill Mills <bill.mills@linaro.org>
2024-10-20 11:57:05 -0400

    demo4: test module removal and test multiple times
    * test that we can remove the module OK
    * do the whole test sequence multiple times (3 here)
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo4.d/mixins/minimal/root/demo4/demo4.sh
+++ demo4.d/mixins/minimal/root/demo4/demo4.sh
@@ -9,14 +9,42 @@ fail() {
 	exit 2
 }
 
-modprobe virtio-msg-ivshmem || fail "can't install module virtio-msg-ivshmem"
+one_test() {
+	NUM=$1
 
-dmesg | grep "virtio_msg_ivshmem .* IRQ fired" >/dev/null || \
-	fail "IRQ did not fire"
-dmesg | grep "virtio_msg_ivshmem .* Data ok." >/dev/null || \
-	fail "Data check did not pass"
-dmesg | grep "virtio_msg_ivshmem .* probe successful" >/dev/null || \
-	fail "module probe did not work"
+	modprobe virtio_msg_ivshmem \
+		|| fail "can't install module virtio-msg-ivshmem (test $NUM)"
+
+	# wait for things to happen
+	sleep 1
+
+	rmmod virtio_msg_ivshmem \
+		|| fail "can't remove module virtio-msg-ivshmem (test $NUM)"
+
+	# this is destructive but busybox dmesg does not have the fancy options
+	dmesg -c >dmesg-test-$NUM.log
+
+	grep "virtio_msg_ivshmem .* IRQ fired" \
+		dmesg-test-$NUM.log  >/dev/null || \
+		fail "IRQ did not fire (test $NUM)"
+	grep "virtio_msg_ivshmem .* Data ok." \
+		dmesg-test-$NUM.log  >/dev/null || \
+		fail "Data check did not pass (test $NUM)"
+	grep "virtio_msg_ivshmem .* probe successful" \
+		dmesg-test-$NUM.log  >/dev/null || \
+		fail "module probe did not work (test $NUM)"
+	grep "virtio_msg_ivshmem .* device removed" \
+		dmesg-test-$NUM.log  >/dev/null || \
+		fail "device was not removed (test $NUM)"
+}
+
+# save the boot log as the tests will read and clear dmesg
+dmesg -c >dmesg-boot.log
+
+for i in $(seq 3); do
+	one_test $i
+	sleep 2
+done
 
 # if we get here, we have passed
 echo "***** TEST PASSED, virtio-msg-ivshmem module test passed"
