2fc6ab784259bded6dc13a651b82356214f7d18f
Bill Mills <bill.mills@linaro.org>
2024-10-29 12:44:13 -0400

    demo4: my work flow stuff
    * create a symlink tree to the important stuff in kernel
    * include kernel working tree in container
    * expose ports from container to host
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

---
+++ work/make-work.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ME=$(readlink -f $0)
+MY_DIR=$(dirname $ME)
+DEMO_BASE=$(dirname $MY_DIR)
+
+mksymdir() {
+	DIRPATH=$1; shift
+	mkdir -p $BASE/$DIRPATH
+
+	for i in $@; do
+		echo ln -fsT $LINK_BASE/$DIRPATH/$i $BASE/$DIRPATH/$i
+		ln -fsT $LINK_BASE/$DIRPATH/$i $BASE/$DIRPATH/$i
+	done
+}
+
+cd $DEMO_BASE/work
+
+LINK_BASE=$DEMO_BASE/../linux-virtio-msg
+BASE=linux-virtio-msg
+mksymdir drivers uio virtio
+mksymdir include linux uapi
+mksymdir arch/arm64 configs
