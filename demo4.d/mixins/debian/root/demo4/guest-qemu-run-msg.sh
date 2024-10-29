e3a3247c68510eeef5ec4f1a798633dc5b0b7a0d
Bill Mills <bill.mills@linaro.org>
2024-10-29 11:48:41 -0400

    demo4: try virtio-rng instead of virtio-net
    rng is simpler, try that.
    With this probe completes but trying to read from /dev/hwrng will hang.
    ^C will cancel the read OK.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo4.d/mixins/debian/root/demo4/guest-qemu-run-msg.sh
+++ demo4.d/mixins/debian/root/demo4/guest-qemu-run-msg.sh
@@ -16,8 +16,10 @@ ${QEMU} ${MACHINE} -m 1G             \
         -object memory-backend-file,id=mem,size=1G,mem-path=${MEMFILE},share=on \
         -serial mon:stdio -display none                         \
 	-device virtio-msg-bus-ivshmem,dev=${DEV_DOORBELL},remote-vmid=${R_VMID},memdev=mem,mem-offset=0x40000000,reset-queues=true \
-        -device virtio-net-device,mq=on,netdev=net0,iommu_platform=off          \
-        -netdev user,id=net0                                                    \
+        -device virtio-rng-device,iommu_platform=on \
         "$@"
+
+#        -device virtio-net-device,mq=on,netdev=net0,iommu_platform=on          \
+#        -netdev user,id=net0                                                    \
 
 exit 0
