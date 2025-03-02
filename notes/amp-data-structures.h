4b99e535eea5090c0426cb45a774667845686fea
Bill Mills <bill.mills@linaro.org>
2025-03-02 18:37:05 -0500

    amp defs: Add notes of fully flexible layout description
    * Add notes / sketch of the full layout model.
    * Move the flexible amp_queue_def_t to after the notes about
      the fully flexible layout model
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- notes/amp-data-structures.h
+++ notes/amp-data-structures.h
@@ -125,45 +125,6 @@ struct amp_notification_t<u8 N, bool mask> {
 	}
 }
 
-/* Queue layout definition
-** This structure can be used in the start of shared memory to define the 
-** shared memory layout for a queue pair.
-**
-** This structure is static at run time from the device & driver POV
-**
-** Use of this structure is not required if both sides can get this information
-** from other places such as devicetree or compile time constants.
-*/
-struct amp_queue_def_t {
-	uint32_t	magic;			// magic & ready indication
-						// ready = MAGIC_Q_DEF
-						// not ready = 0 or MAGIC_Q_DEF_NOT_READY
-	uint32_t	version;		// 0x0001_0000
-	uint32_t	driver_peer_ord;	// should be 0 if not used
-	uint32_t	device_peer_ord;	// should be 1 if not used
-
-	/* size and count of queue elements from driver to device
-	** element size should be a power of 2,
-	**    some implementations may only support a given size such as 64
-	** number of elements should be > 1 */
-	uint16_t	driver_element_size;
-	uint16_t	driver_num_elements;
-
-	/* size and count of queue elements from device to driver
-	** same constraints as driver to device */
-	uint16_t	device_element_size;
-	uint16_t	device_num_elements;
-
-	/* offsets from the start of the containing memory area
-	** each head is of type amp_queue_head_t
-	** each data is u8 data[num_elements][element_size]
-	*/
-	uint64_t	driver_head;
-	uint64_t	driver_data;
-	uint64_t	device_head;
-	uint64_t	device_data;
-}
-
 /* Example Layout, (medium)
 **
 ** Driver Segment 4k, driver r/w, device ro
@@ -307,4 +268,90 @@ struct amp_layout_def_small_t {
 	uint64_t	off_drv_q_elements;
 	uint32_t	size_dev_q_elements;
 	uint32_t	size_drv_q_elements;
+}
+
+/* Full flexibility layout definition
+
+I need to find the right terms and I have more data structures to add but here is the idea:
+
+I expect one or more share memory areas. Example:
+	one area somewhere in DDR and one in OCRAM
+
+Each memory area will have a data structure that gives it an ID and defines sub-regions.
+Each sub-region will define which peers have what r/w permissions and who has allocate ownership.
+
+The 4k below is just an example, it could be 256 bytes or 1k or whatever fits the data.
+Memory permissions can be the honor system or can be enforced by HW or hypervisors etc.
+If enforced the size of the sub-areas will need to be adjusted to fit.
+
+This sketch shows TWO virtio-msg buses:
+	Linux driver and M4 device
+	M4 driver and Linux device
+
+DDR shared memory segment:
+4K coordinator owned data, ro for peer 0  & 1
+	memory area header 	// header for this memory area
+	memory ID table		// table of all memory areas
+	memory sub-area table	// sub-area list for this memory area
+	peer ID table		// list of peers
+	device table		// list of virtio-msg buses and other devices
+4k peer 0 (Linux) owned data, rw by peer 1, ro peer 0
+	peer 0 info structures
+	queue heads and queue data for Linux driver to M4 device
+	queue heads and queue data for M4 driver to Linux device
+4k peer 1 (M4) owned data, rw by peer 1, ro peer 0
+	peer 1 info structures
+	queue heads and queue data for M4 device to Linux driver
+	queue heads and queue data for M4 driver to Linux device
+2M-12K
+	r/w by Linux and M4, allocation owner = M4
+	M4 allocates virt-queues and buffers here for its drivers
+8M
+	r/w by Linux and M4, allocation owner = Linux
+	Linux allocates virt-queues and buffers here for its drivers
+
+
+Shared OCRAM:
+	memory area header
+	r/w by Linux and M4, allocation owner = Linux
+
+*/
+
+/* Queue layout definition
+** This structure can be used in the start of shared memory to define the
+** shared memory layout for a queue pair.
+**
+** This structure is static at run time from the device & driver POV
+**
+** Use of this structure is not required if both sides can get this information
+** from other places such as devicetree or compile time constants.
+*/
+struct amp_queue_def_t {
+	uint32_t	magic;			// magic & ready indication
+						// ready = MAGIC_Q_DEF
+						// not ready = 0 or MAGIC_Q_DEF_NOT_READY
+	uint32_t	version;		// 0x0001_0000
+	uint32_t	driver_peer_ord;	// should be 0 if not used
+	uint32_t	device_peer_ord;	// should be 1 if not used
+
+	/* size and count of queue elements from driver to device
+	** element size should be a power of 2,
+	**    some implementations may only support a given size such as 64
+	** number of elements should be > 1 */
+	uint16_t	driver_element_size;
+	uint16_t	driver_num_elements;
+
+	/* size and count of queue elements from device to driver
+	** same constraints as driver to device */
+	uint16_t	device_element_size;
+	uint16_t	device_num_elements;
+
+	/* offsets from the start of the containing memory area
+	** each head is of type amp_queue_head_t
+	** each data is u8 data[num_elements][element_size]
+	*/
+	amp_memref_t	driver_head;
+	amp_memref_t	driver_data;
+	amp_memref_t	device_head;
+	amp_memref_t	device_data;
 }
