293c661c469240fbac89f64e7811d97dc6c77194
Bill Mills <bill.mills@linaro.org>
2025-03-09 23:45:45 -0400

    mixins/minimal: make autorun work with any console
    * Move logic of tty selection to autorun.sh from .profile
    * Add autorun_tty param to set which tty to run on
    * Use first console if autorun_tty not specified
    * Add autopoweroff to control poweroff after run
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- mixins/minimal/root/autorun.sh
+++ mixins/minimal/root/autorun.sh
@@ -39,9 +39,26 @@ delay() {
 
 AUTORUN=$(get_cmdline_arg autorun "")
 AUTORUN_DELAY=$(get_cmdline_arg autorun_delay 5)
+AUTOPOWEROFF=$(get_cmdline_arg autopoweroff "yes")
+AUTORUN_TTY=$(get_cmdline_arg autorun_tty "")
+PRIME_CONSOLE=$(cat /proc/consoles | cut -d" " -f 1 | head -n 1)
+
+MY_TTY=$(tty)
+MY_TTY=${MY_TTY#/dev/}
+
+if [ -n "$AUTORUN_TTY" ]; then
+	if [ "$MY_TTY" != $AUTORUN_TTY ]; then
+		exit 0
+	fi
+else
+	if [ "$MY_TTY" != $PRIME_CONSOLE ]; then
+		exit 0
+	fi
+fi
 
 echo "/proc/cmdline: $(cat /proc/cmdline)"
-echo "AUTORUN=$AUTORUN AUTORUN_DELAY=$AUTORUN_DELAY"
+echo "MY_TTY=$MY_TTY AUTORUN_TTY=$AUTORUN_TTY PRIME_CONSOLE=$PRIME_CONSOLE"
+echo "AUTORUN=$AUTORUN AUTORUN_DELAY=$AUTORUN_DELAY AUTOPOWEROFF=$AUTOPOWEROFF"
 
 if [ -z "$AUTORUN" ]; then
 	exit
@@ -59,8 +76,20 @@ else
     AUTORUN_DELAY=30
 fi
 
+case $AUTOPOWEROFF in
+on|ON|yes|YES|y|Y|"")
+	true
+	;;
+off|OFF|no|NO|n|N)
+	exit
+	;;
+0*|1*|2*|3*|4*|5*|6*|7*|8*|9*|0*)
+	AUTORUN_DELAY=$(( $AUTOPOWEROFF + 0 ))
+	;;
+esac
+
 echo "To stop poweroff, hit ctrl-c in the next $AUTORUN_DELAY seconds"
 delay $AUTORUN_DELAY
 
-echo "busybox poweroff"
+echo "minimal poweroff"
 poweroff
