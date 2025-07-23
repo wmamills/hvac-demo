8defbe6f51a204975a31dd377494ecee1a150f45
Bill Mills <bill.mills@linaro.org>
2025-07-23 15:12:49 -0400

    loopback: don't install xen & qemu stuff
    This demo does not need xen nor qemu in the image.
    This is proven to work with out this so remove it.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo-loopback.d/mixins/debian/root/demo-loopback/install.sh
+++ demo-loopback.d/mixins/debian/root/demo-loopback/install.sh
@@ -8,12 +8,6 @@ fi
 echo "Installing packages ..."
 apt-get update -qq
 apt-get install -qqy procps nano
-apt-get install -qqy libyajl2 zlib1g libfdt1 libncurses5 libzstd1 libuuid1 \
-    libpixman-1-0 libslirp0
-apt-get install ./xen-ffa.deb
-
-# use /opt/qemu-upstream as /opt/qemu
-ln -fs -T qemu-ffa /opt/qemu
 
 chmod +x lb-vhost-device-* lb-vhost-frontend virtio-msg-loopback
 
