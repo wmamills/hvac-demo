0dd58e7d3766573242e07986ba82871b0c2ebffa
Bill Mills <bill.mills@linaro.org>
2025-07-26 10:54:02 -0400

    loopback: generate the dtb dynamically at runtime
    Generate the dtb needed by QEMU dynamically at runtime.
    
    This is better because:
    * The dtb is very specific to the QEMU arguments
    * This allows us to adjust the QEMU arguments easily
    * It shows just the actual change needed in the dtb in text format
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo-loopback.d/scripts/common-vars.sh
+++ demo-loopback.d/scripts/common-vars.sh
@@ -7,3 +7,15 @@ QEMU_DIR=host/${HOST_ARCH}/qemu-i2c
 QEMU=$IMAGES/$QEMU_DIR/bin/qemu-system-aarch64
 
 KERNEL=target/aarch64/linux-virtio-msg-lb-Image
+
+QEMU_BASE=(
+  -machine virt,virtualization=on,gic-version=3
+  -cpu cortex-a57 -serial mon:stdio
+  -device virtio-net-pci,netdev=net0 -netdev user,id=net0,hostfwd=tcp::8022-:22
+  -drive file=${BUILD}/demo-loopback-disk.qcow2,id=hd0,if=none,format=qcow2
+  -device virtio-scsi-pci -device scsi-hd,drive=hd0
+  -display none -m 8192 -smp 8
+  -kernel ${IMAGES}/${KERNEL}
+  -append "earlycon root=/dev/sda2 autorun=./demo-loopback/demo.sh"
+  -device ds1338,address=0x20
+)
--- demo-loopback.d/scripts/setup.sh
+++ demo-loopback.d/scripts/setup.sh
@@ -7,3 +7,17 @@ MY_DIR=$(dirname $ME_ABS)
 
 $FETCH --image $QEMU_DIR $KERNEL
 copy_debian_disk demo-loopback
+
+# Create DTB to use
+# Get the dtb for the configuration we are going to use
+${QEMU} \
+	"${QEMU_BASE[@]}" \
+	-machine dumpdtb=$MY_DIR/qemu.dtb
+
+# get the source for debug
+dtc -I dtb -O dts -o $MY_DIR/qemu.dts $MY_DIR/qemu.dtb
+
+# poorman's overlay
+# adapted from https://docs.u-boot.org/en/latest/develop/devicetree/dt_qemu.html
+cat  $MY_DIR/qemu.dts $MY_DIR/reserved-memory.dts >$MY_DIR/merged.dts
+dtc -o $MY_DIR/merged.dtb $MY_DIR/merged.dts
