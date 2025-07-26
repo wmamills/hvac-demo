6e2fb3e60e0b63306dbdc326e151c2d7ccb81833
Bill Mills <bill.mills@linaro.org>
2025-07-26 10:54:02 -0400

    loopback: generate the dtb dynamically at runtime
    Generate the dtb needed by QEMU dynamically at runtime.
    
    This is better because:
    * The dtb is very specific to the QEMU arguments
    * This allows us to adjust the QEMU arguments easily
    * It shows just the actual change needed in the dtb in text format
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- demo-loopback.d/scripts/run-A.sh
+++ demo-loopback.d/scripts/run-A.sh
@@ -6,12 +6,5 @@ MY_DIR=$(dirname $ME_ABS)
 . $MY_DIR/common-vars.sh
 
 ${QEMU} \
-  -machine virt,virtualization=on,gic-version=3 -cpu cortex-a57 -serial mon:stdio \
-  -device virtio-net-pci,netdev=net0 -netdev user,id=net0,hostfwd=tcp::8022-:22 \
-  -drive file=${BUILD}/demo-loopback-disk.qcow2,id=hd0,if=none,format=qcow2 \
-  -device virtio-scsi-pci -device scsi-hd,drive=hd0 \
-  -display none -m 8192 -smp 8 \
-  -kernel ${IMAGES}/${KERNEL} \
-  -append "earlycon root=/dev/sda2 autorun=./demo-loopback/demo.sh" \
-  -device ds1338,address=0x20 \
-  -dtb $MY_DIR/qemu.dtb
+	"${QEMU_BASE[@]}" \
+	-dtb $MY_DIR/merged.dtb
